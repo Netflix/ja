fd0cd7d6fe1630360ca09dd15864191b9a18ad2e
Danny Thomas <dannyt@netflix.com>
2026-09-16 17:03:23 +1000

    Authorize jdocserver access in tests

--- src/com.netflix.tools.ja.test/module-info.java
+++ src/com.netflix.tools.ja.test/module-info.java
@@ -12,6 +12,15 @@
  * the License.
  */
 
+/**
+ * @addExports jdk.javadoc/jdk.javadoc.internal.doclets.formats.html=com.netflix.tools.jdocserver
+ * @addExports jdk.javadoc/jdk.javadoc.internal.api=com.netflix.tools.jdocserver
+ * @addExports jdk.javadoc/jdk.javadoc.internal.doclets.toolkit.util=com.netflix.tools.jdocserver
+ * @addExports jdk.javadoc/jdk.javadoc.internal.tool=com.netflix.tools.jdocserver
+ * @addExports jdk.compiler/com.sun.tools.javac.api=com.netflix.tools.jdocserver
+ * @addExports jdk.compiler/com.sun.tools.javac.main=com.netflix.tools.jdocserver
+ * @addExports jdk.compiler/com.sun.tools.javac.util=com.netflix.tools.jdocserver
+ */
 open module com.netflix.tools.ja.test {
     requires com.netflix.tools.cli;
     requires com.netflix.tools.ja;
