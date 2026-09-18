1d3bebd310fdb7b096f65aecb61169f5ebd39bda
Danny Thomas <dannyt@netflix.com>
2026-09-18 16:41:51 +1000

    Refresh dependencies with ja 0.21.1

--- src/com.netflix.tools.ja/module-info.java
+++ src/com.netflix.tools.ja/module-info.java
@@ -20,7 +20,7 @@ module com.netflix.tools.ja {
     exports com.netflix.tools.ja to com.netflix.tools.ja.test;
 
     requires com.netflix.tools.cli;
-    requires com.netflix.tools.jig; // @0.16.1
+    requires com.netflix.tools.jig; // @0.16.3
     requires com.netflix.tools.launcher;
     requires java.compiler;
     requires java.xml;
